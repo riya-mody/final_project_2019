64c16f8e0385ca57de02f66c5d348953ef14b72b
riya-mody <mody@chapman.edu>
2019-11-19 12:44:20 -0800

    "Cleaning out dataset"

--- final project script.R
+++ final project script.R
@@ -1,3 +1,6 @@
+rm(list=ls())
 setwd("~/Desktop/comp sci project datasets")
 data.facial<-read.csv("facial_expression.csv")
+data.facial[data.facial=="FIT_FAILED"]<-NA
+data.facial[data.facial=="FIND_FAILED"]<-NA
 data.facial2<-na.omit(data.facial)
