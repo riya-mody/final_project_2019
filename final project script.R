31cfb38e30fcfead9fc773145edc105e67385df4
riya-mody <mody@chapman.edu>
2019-11-21 15:32:51 -0800

    "Changed dataset and did some inital plotting"

--- final project script.R
+++ final project script.R
@@ -2,17 +2,12 @@ rm(list=ls())
 
 setwd("~/Desktop/comp sci project datasets")
 
-data.facial<-read.csv("facial_expression.csv")
-data.facial[data.facial=="FIT_FAILED"]<-NA
-data.facial[data.facial=="FIND_FAILED"]<-NA
-data.facial2<-na.omit(data.facial)
+happiness<-read.csv("2017.csv")
 
-library(ggplot2)
+plot(happiness$Happiness.Score, happiness$Family) #Correlation btwn happiness and family
 
-pdf("bad.graph.pdf")
-G<-ggplot(data.facial2, aes(x=Video.Time, y=Happy)) + geom_point() + 
-  ggtitle("Happiness over Time") +
-  xlab("Video Time") +
-  ylab("Happiness Levels")
-G
-dev.off()
+happyfamily.lm<-lm(Family~Happiness.Score,data=happiness) #Linear model
+summary(happyfamily.lm) #Shows stats
+abline(happyfamily.lm,col="red") #Inserts regression line for correlation 
+
+268 #Page for correlation 
