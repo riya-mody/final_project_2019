550db68408bf038067a7cafd27ea66b0adc65a1d
riya-mody <mody@chapman.edu>
2019-11-21 01:02:54 -0800

    "Making first attempt at graphing --> failed"

--- final project script.R
+++ final project script.R
@@ -1,6 +1,18 @@
 rm(list=ls())
+
 setwd("~/Desktop/comp sci project datasets")
+
 data.facial<-read.csv("facial_expression.csv")
 data.facial[data.facial=="FIT_FAILED"]<-NA
 data.facial[data.facial=="FIND_FAILED"]<-NA
 data.facial2<-na.omit(data.facial)
+
+library(ggplot2)
+
+pdf("bad.graph.pdf")
+G<-ggplot(data.facial2, aes(x=Video.Time, y=Happy)) + geom_point() + 
+  ggtitle("Happiness over Time") +
+  xlab("Video Time") +
+  ylab("Happiness Levels")
+G
+dev.off()
